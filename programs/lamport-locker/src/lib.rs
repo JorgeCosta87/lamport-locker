02a81fd339e46083e02d1d508f8ae09d98ee4050
equilibrium <jorgemdcosta@gmail.com>
2025-10-31 11:07:04 +0000

    Add deposit and withdrawn instructions

--- programs/lamport-locker/src/lib.rs
+++ programs/lamport-locker/src/lib.rs
@@ -4,6 +4,7 @@ declare_id!("Fsqxbxgpm1rP2gWoXxmm5otdJmjCBE2GG39G1TaL9u4K");
 
 mod instructions;
 mod state;
+mod errors;
 
 pub use instructions::*;
 
@@ -13,6 +14,18 @@ pub mod lamport_locker {
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         ctx.accounts.initialize(ctx.bumps)?;
+
+        Ok(())
+    }
+
+    pub fn deposit(ctx: Context<Vault>, amount: u64) -> Result<()> {
+        ctx.accounts.deposit(amount)?;
+
+        Ok(())
+    }
+
+    pub fn withdraw(ctx: Context<Vault>, amount: u64) -> Result<()> {
+        ctx.accounts.withdraw(amount)?;
 
         Ok(())
     }
